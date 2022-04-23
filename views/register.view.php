c5a67768193b9432a2609f0ea4630c9e3679bf49
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-04-23 03:22:14 -0500

    error css polish

--- views/register.view.php
+++ views/register.view.php
@@ -20,73 +20,72 @@ include __DIR__ . '/inc/header.inc.php';
         <p>
             <label for="first_name">First Name</label>
             <input type="text" name="first_name" value="<?= esc($_POST['first_name'] ?? '') ?>">
-            <span class="error"><?= esc($errors['first_name'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['first_name'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="last_name">Last Name</label>
             <input type="text" name="last_name" value="<?= esc($_POST['last_name'] ?? '') ?>">
-            <span class="error"><?= esc($errors['last_name'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['last_name'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="street">Street</label>
             <input type="text" name="street" value="<?= esc($_POST['street'] ?? '') ?>">
-            <span class="error"><?= esc($errors['street'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['street'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="city">City</label>
             <input type="text" name="city" value="<?= esc($_POST['city'] ?? '') ?>">
-            <span class="error"><?= esc($errors['city'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['city'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="postal_code">Postal Code</label>
             <input type="text" name="postal_code" value="<?= esc($_POST['postal_code'] ?? '') ?>">
-            <span class="error"><?= esc($errors['postal_code'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['postal_code'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="province">Province</label>
             <input type="text" name="province" value="<?= esc($_POST['province'] ?? '') ?>">
-            <span class="error"><?= esc($errors['province'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['province'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="country">Country</label>
             <input type="text" name="country" value="<?= esc($_POST['country'] ?? '') ?>">
-            <span class="error"><?= esc($errors['country'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['country'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="phone">Phone</label>
             <input type="text" name="phone" value="<?= esc($_POST['phone'] ?? '') ?>">
-            <span class="error"><?= esc($errors['phone'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['phone'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="email">Email</label>
             <input type="text" name="email" value="<?= esc($_POST['email'] ?? '') ?>">
-            <span class="error"><?= esc($errors['email'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['email'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="password">Password</label>
             <input type="password" name="password" value="">
-            <span class="error"><?= esc($errors['password'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['password'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="password_confirm">Input password again</label>
             <input type="password" name="password_confirm" value="">
-            <span class="error"><?= esc($errors['password_confirm'][0] ?? '') ?></span>
+            <span class="form_validate_error"><?= esc($errors['password_confirm'][0] ?? '') ?></span>
         </p>
 
         <p>
             <label for="subscribe_to_newsletter">Subscribe to newsletter</label>
-            <input type="checkbox" name="subscribe_to_newsletter"
-                value="<?= esc($_POST['subscribe_to_newsletter'] ?? 1) ?>">
+            <input type="checkbox" name="subscribe_to_newsletter" value="<?= esc($_POST['subscribe_to_newsletter'] ?? 1) ?>">
         </p>
 
         <p><button type="submit" style="margin-left: 180px;">Submit</button></p>
