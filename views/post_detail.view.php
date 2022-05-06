357460c65acf48b9d20015396973225ec16e37c9
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-05-06 11:39:22 -0500

    post detail page blueprint done

--- views/post_detail.view.php
+++ views/post_detail.view.php
@@ -1,8 +1,35 @@
 <?php include __DIR__ . '/inc/header.inc.php'; ?>
 
 <div class="main_wrapper">
-    <h1><?= e($title) ?></h1>
 
+    <div>Post Title</div>
+    <div>
+        <div><img src="" alt=""></div>
+        <div>Author Name</div>
+        <div>Post Date</div>
+        <div>Comment Count</div>
+    </div>
+    <div>
+        <div><img src="" alt=""></div>
+        <div>Post Content</div>
+    </div>
+    <div>
+        <div>
+            <div>More Article Title</div>
+            <div>More Article Date</div>
+        </div>
+    </div>
+    <div>category list</div>
+    <div>
+        <form action="" method="post">
+            <h1>Please Leave Your Comment</h1>
+            <div>
+                <p><label for="textarea_comment">Comment</label></p>
+                <textarea name="content" id="textarea_comment" cols="30" rows="10"></textarea>
+            </div>
+            <p><button type="submit">Send</button></p>
+        </form>
+    </div>
 </div>
 
 <?php include __DIR__ . '/inc/footer.inc.php'; ?>
