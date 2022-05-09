75669e91db363c6f46cf69fe189734ebbf98276e
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-05-09 11:48:25 -0500

    post detail controller revise

--- controllers/post.php
+++ controllers/post.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Models\Post;
+
 $title = 'Post';
 
 if (!empty($_GET['postid'])) {
-    view('post_detail', compact('title'));
+
+    // new a post object
+
+    $post = new Post($dbh);
+
+    $post_detail = $post->getOne($_GET['postid']);
+
+    view('post_detail', compact('title', 'post_detail'));
 } else {
     view('post', compact('title'));
 }
