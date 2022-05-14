023a3ae2a77d5be76fd114eee0b91a3e6277e1b7
Leon Johnson <yao-l@webmail.uwinnipeg.ca>
2022-05-13 23:24:09 -0500

    post_create.php init

--- public/admin/post_create.php
+++ public/admin/post_create.php
@@ -2,13 +2,16 @@
 
 require __DIR__ . '/config/config.php';
 // ----- SET PAGE TITLE ------------------------
-$title = 'Post | Administration';
+$title = 'Posts | Administration';
 
 /*--------------------------------------------------------------------------*/
 
 /*-----------             ACTUAL CODE IN THIS PAGE              ------------*/
 
 /*--------------------------------------------------------------------------*/
+
+$user_dtls = $user->getAll();
+$cat_dtls = $cat->getAll();
 
 include __DIR__ . '/inc/header.inc.php';
 
@@ -27,7 +30,121 @@ include __DIR__ . '/inc/header.inc.php';
     <?php endif; ?>
 
     <h1 class="mb-5">Create a New Post</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-2"> </div>
+            <div class="col-8">
+                <form method="post" enctype="multipart/form-data" novalidate>
+                    <input type="hidden" name="csrf_token" value="<?= csrf() ?>" />
+                    <div class="mb-3">
+                        <label for="title" class="form-label fw-bold">Title</label>
+                        <input type="text" class="form-control" id="title" name="title"
+                            value="<?= esc($_POST['title'] ?? '') ?>">
+                        <span class="form_validate_error my-3"><?= esc($errors['title'][0] ?? '') ?></span>
+                    </div>
+                    <div class="mb-3">
+                        <label for="summary" class="form-label fw-bold">Summary</label>
+                        <textarea class="form-control" id="summary" name="summary" rows="3"
+                            placeholder="Add a summary on this post..."><?= esc($_POST['summary'] ?? ''); ?></textarea>
+                        <span class="form_validate_error my-3"><?= esc($errors['summary'][0] ?? '') ?></span>
+                    </div>
+                    <div class="mb-3">
+                        <label for="content" class="form-label fw-bold">Content</label>
+                        <textarea class="form-control editor" id="content" name="content"
+                            rows="3"><?= esc($_POST['content'] ?? ''); ?></textarea>
+                        <span class="form_validate_error my-3"><?= esc($errors['content'][0] ?? '') ?></span>
+                    </div>
 
+                    <div class="row mb-3">
+                        <div class="col">
+                            <label for="authorid" class="form-label fw-bold">Author</label>
+                            <select class="form-select" id="authorid" name="authorid">
+                                <option value="0" selected>Select your author</option>
+                                <?php foreach ($user_dtls as $key => $user_dtl) : ?>
+                                <option value="<?= esc_attr($user_dtl['id']) ?>">
+                                    <?= esc($user_dtl['first_name'] . ' ' . $user_dtl['last_name']) ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <span class="form_validate_error my-3"><?= esc($errors['authorid'][0] ?? '') ?></span>
+                        </div>
+                        <div class="col">
+                            <label for="categoryid" class="form-label fw-bold">Category</label>
+                            <select class="form-select" id="categoryid" name="categoryid">
+                                <option value="0" selected>Select category</option>
+                                <?php foreach ($cat_dtls as $key => $cat_dtl) : ?>
+                                <option value="<?= esc_attr($cat_dtl['id']) ?>">
+                                    <?= esc($cat_dtl['title']) ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                            <span class="form_validate_error my-3"><?= esc($errors['categoryid'][0] ?? '') ?></span>
+                        </div>
+                        <div class="col">
+                            <label for="status" class="form-label fw-bold">Status</label>
+                            <select class="form-select" id="status" name="status">
+                                <option value="0" selected>Select status</option>
+                                <option value="draft">draft</option>
+                                <option value="hidden">hidden</option>
+                                <option value="post">post</option>
+                            </select>
+                            <span class="form_validate_error my-3"><?= esc($errors['status'][0] ?? '') ?></span>
+                        </div>
+                        <div class="col">
+                            <label for="allow_comment" class="form-label fw-bold">Allow Comment</label>
+                            <select class="form-select" id="allow_comment" name="allow_comment">
+                                <option value="1" selected>Yes</option>
+                                <option value="0">No</option>
+                            </select>
+                            <span class="form_validate_error my-3"><?= esc($errors['allow_comment'][0] ?? '') ?></span>
+                        </div>
+
+
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label fw-bold">Upload a picture</label>
+                        <input class="form-control" type="file" id="formFile">
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+
+            </div>
+            <div class="col-2"> </div>
+
+        </div>
+    </div>
 </div>
 
 <?php include __DIR__ . '/inc/footer.inc.php'; ?>
+
+<script src="./js/ckeditor.js"></script>
+<script>
+ClassicEditor
+    .create(document.querySelector('.editor'), {
+
+        licenseKey: '',
+
+
+
+    })
+    .then(editor => {
+        window.editor = editor;
+
+
+
+
+    })
+    .catch(error => {
+        console.error('Oops, something went wrong!');
+        console.error(
+            'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:'
+        );
+        console.warn('Build id: org5kicul2kg-rzn5b6r6hoyd');
+        console.error(error);
+    });
+</script>
+
+</body>
+
+</html>
